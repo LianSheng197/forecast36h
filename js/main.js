8267074b3afca79e2f227b59f632b21933f9610e
LianSheng197 <ci.i.cxcvii@gmail.com>
2020-06-04 00:31:18 +0800

    增加預處理時段 _periodCheckPoint

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let _metaData = {};
 let _sortedData = [];
+let _periodCheckPoint = [];
 
 (async function () {
     // 取得預報資料
@@ -20,6 +21,28 @@ let _sortedData = [];
             "provider": forecastData.sender,
             "updateTime": forecastData.sent
         };
+
+        // 處理 _periodCheckPoint
+        let d = new Date(_metaData.updateTime);
+        if (d.getHours() <= 12) {
+            d.setHours(12);
+            _periodCheckPoint.push(parseDate(d));
+            d = addHours(d, 6);
+            _periodCheckPoint.push(parseDate(d));
+
+            for (let i = 0; i < 2; i++) {
+                d = addHours(d);
+                _periodCheckPoint.push(parseDate(d));
+            }
+        } else {
+            d.setHours(18);
+            _periodCheckPoint.push(parseDate(d));
+
+            for (let i = 0; i < 3; i++) {
+                d = addHours(d);
+                _periodCheckPoint.push(parseDate(d));
+            }
+        }
 
         // 資料預處理，並簡化格式
         // 各個屬性的 time 部份因固定拆分成三區段
@@ -46,11 +69,31 @@ let _sortedData = [];
 
         console.log(_metaData);
         console.log(_sortedData);
+        console.log(_periodCheckPoint);
     } else {
         alert("取得預報資料時出錯，請重新整理");
         return;
     }
 }());
+
+// 解析日期
+function parseDate(date) {
+    let m = date.getMonth() + 1;
+    let d = date.getDate();
+    let h = date.getHours();
+
+    m = `0${m}`.substr(-2);
+    d = `0${d}`.substr(-2);
+    h = `0${h}`.substr(-2);
+
+    return `${m}/${d} ${h}:00:00`;
+}
+
+// 增加小時
+function addHours(date, hours=12) {
+    return new Date(date.getTime() + hours * 60 * 60 * 1000);
+}
+
 
 
 // 觸發導入片段檔的事件註冊
@@ -72,7 +115,7 @@ async function insertPartialHTML(filename) {
 
     // 添加非 script 的 HTML
     content.innerHTML = html.replace(/<script>[^]+?<\/script>/, "");
-    
+
 
     // 添加 script
     let script = html.match(/<script>([^]+?)<\/script>/);
@@ -80,14 +123,14 @@ async function insertPartialHTML(filename) {
     if (script != null) {
         let s = document.createElement('script');
         s.type = "text/javascript";
-        
+
         s.innerHTML = `
         (function(){
             ${script[1]}
         })();
         `;
         content.appendChild(s);
-    }    
+    }
 }
 
 insertPartialHTML("index");
